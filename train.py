c9b6122f49766439b90948019b15424ac9b9daf1
Andrew Craig <andy.peter.craig@gmail.com>
2020-05-17 08:48:38 +0900

    Add param beta, group mlflow code

--- train.py
+++ train.py
@@ -2,21 +2,21 @@ import argparse
 import random
 import mlflow
 
-def main(alpha):
+def main(alpha, beta):
+    params = {'alpha': alpha, 'beta': beta}
+    loss = random.random()
+    print(f"loss: {loss}")
+    with open("output.txt", 'w') as f:
+        f.write("Finished!")
     with mlflow.start_run():
-        mlflow.log_param("alpha", alpha)
-
-        loss = random.random()
-        print(f"loss: {loss}")
+        mlflow.log_params(params)
         mlflow.log_metric("loss", loss)
-
-        with open("output.txt", 'w') as f:
-            f.write("Finished!")
         mlflow.log_artifact("output.txt")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('alpha', type=float)
+    parser.add_argument('beta', type=float)
 
     args = parser.parse_args()
-    main(args.alpha)
+    main(args.alpha, args.beta)
